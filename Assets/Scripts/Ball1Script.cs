9c80742109f4283639fbf2a708991050fa408903
cohensam <cohensam@seas.upenn.edu>
2014-11-12 01:57:21 -0500

    Sam's scene

---
+++ Assets/Scripts/Ball1Script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball1Script : MonoBehaviour {
+
+	private bool right;
+
+	// Use this for initialization
+	void Start () {
+		right = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x <= 20) {
+			right = true;
+		}
+
+		if (transform.position.x >= 34) {
+			right = false;
+		}
+
+		if (right) {
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+		} else {
+			transform.Translate(-Vector2.right * 4f * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Ball2Script.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball2Script : MonoBehaviour {
+
+	private bool up;
+
+	// Use this for initialization
+	void Start () {
+		up = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y <= -2.2) {
+			up = true;
+		}
+
+		if (transform.position.y >= 2.8) {
+			up = false;
+		}
+		
+		if (up) {
+			transform.Translate(Vector2.up* 4f * Time.deltaTime);
+		} else {
+			transform.Translate(-Vector2.up * 4f * Time.deltaTime);
+		}
+	}
+}
