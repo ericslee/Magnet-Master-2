e8435cde32e6723c7dae4e8f8fac6e28707baa8b
cohensam <cohensam@seas.upenn.edu>
2014-11-14 19:35:50 -0500

    Added temp textures to SamScene as well as a new conveyor belt that works with a script.

---
+++ Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.back * 4f * Time.deltaTime);
+	}
+}
