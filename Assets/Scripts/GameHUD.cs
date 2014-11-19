085c03d73bc78ec69f1b2ad94cf4c28ba4540d7b
Judy Trinh <jtrinh74@gmail.com>
2014-11-19 10:49:24 -0500

    Added gains bar

--- Assets/Scripts/GameHUD.cs
+++ Assets/Scripts/GameHUD.cs
@@ -8,6 +8,8 @@ public class GameHUD : MonoBehaviour {
 
 	Texture2D healthBar;
 	Texture2D healthBarFill;
+	Texture2D gainsBar;
+	Texture2D gainsBarFill;
 
 	void Start () 
 	{
@@ -17,6 +19,8 @@ public class GameHUD : MonoBehaviour {
 
 		healthBar = Resources.Load("Textures/health-bar") as Texture2D;
 		healthBarFill = CreateHealthBarTexture();
+		gainsBar = Resources.Load("Textures/gains-bar") as Texture2D;
+		gainsBarFill = CreateGainsBarTexture();
 	}
 	
 	void Update () {
@@ -46,6 +50,21 @@ public class GameHUD : MonoBehaviour {
 		return hBarFill;
 	}
 
+	private Texture2D CreateGainsBarTexture() {
+		int width = gainsBar.width;
+		int height = gainsBar.height;
+		
+		Texture2D gBarFill = new Texture2D(width, height);
+		Color[] colors = gBarFill.GetPixels();
+		for (int i = 0; i < colors.Length; i++) {
+			colors[i] = GetColorFrom256Scale(177, 6, 57, 0.5f);
+		}
+		gBarFill.SetPixels(colors);
+		gBarFill.Apply();
+		
+		return gBarFill;
+	}
+
 	// Right now this is hooked to num lives, will have to change later
 	private void DrawHealthBar() {
 		float healthRatio = (float)gameManager.GetTotalLives() / gameManager.GetMaxLives();
@@ -65,6 +84,27 @@ public class GameHUD : MonoBehaviour {
 		GUI.BeginGroup(new Rect(10, (int)(Screen.height * 0.03f), w, h));
 			GUI.DrawTexture(new Rect(0, 0, w, h), healthBar);
 			GUI.DrawTexture(new Rect(x, y, (int)((w - x * 2) * healthRatio), h - y * 2), healthBarFill); 
+		GUI.EndGroup();
+	}
+
+	private void DrawGainsBar() {
+		float gainsRatio = 0.5f; //HOOK TO GAIN
+		
+		// Fraction of the screen width we want the health bar to take up
+		float mult = 0.65f;
+		
+		// New width and height of health bar on screen w.r.t our Screen size
+		int h = (int)(Screen.height * mult);
+		int w = (int)(h * gainsBar.width / gainsBar.height);
+		
+		// x,y where the health bar fill starts (top left corner of it). 7 comes from the full size border being 7 pixels.
+		// +1 because it usually looks better with that buffer.
+		int x = (int)((float)h / gainsBar.height * 7) + 1;
+		int y = (int)((float)w /gainsBar.width * 7) + 1;
+		
+		GUI.BeginGroup(new Rect(10, (int)(Screen.height * 0.2f), w, h));
+		GUI.DrawTexture(new Rect(0, 0, w, h), gainsBar);
+		GUI.DrawTexture(new Rect(x, y, w - x * 2, (int)((h - y * 2) * gainsRatio)), gainsBarFill); 
 		GUI.EndGroup();
 	}
 
@@ -88,5 +128,6 @@ public class GameHUD : MonoBehaviour {
 
 		// Health
 		DrawHealthBar();
+		DrawGainsBar();
 	}
 }
