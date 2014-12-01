f805c5c693b5db5de6a604069b365902d4c56214
Judy Trinh <jtrinh74@gmail.com>
2014-12-01 10:04:38 -0500

    Replaced conveyor

--- Assets/Scripts/EnvironmentScripts/ConveyorScript.cs
+++ Assets/Scripts/EnvironmentScripts/ConveyorScript.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class ConveyorScript : MonoBehaviour {
+	Vector3 rotation;
 
 	void Start() 
 	{
-	
+		rotation = new Vector3(-1.0f,0,0);
 	}
 	
 	void Update() 
 	{
-		transform.Rotate(Vector3.back * 10f * Time.deltaTime);
+		transform.Rotate(rotation * 10f * Time.deltaTime);
 	}
 }
