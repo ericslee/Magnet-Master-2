6b9db176a99230f5c60fccdf6d0742475b0ecc61
Eric Lee <ericslee@seas.upenn.edu>
2014-12-07 14:45:30 -0500

    Level 2 1st puzzle

---
+++ Assets/Scripts/EnvironmentScripts/BreakableFloorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableFloorScript : MonoBehaviour {
+
+	void Start() 
+	{
+	
+	}
+	
+	void Update() 
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name.Equals("Weight"))
+		{
+			if (collision.relativeVelocity.magnitude > 8)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
