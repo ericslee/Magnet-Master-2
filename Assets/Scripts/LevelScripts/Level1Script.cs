6788f906b5ae938b0ddbaeded79f42cb44086b6d
Eric Lee <ericslee@seas.upenn.edu>
2014-12-07 00:53:34 -0500

    Tutorial text flying in

--- Assets/Scripts/LevelScripts/Level1Script.cs
+++ Assets/Scripts/LevelScripts/Level1Script.cs
@@ -13,15 +13,35 @@ public class Level1Script : MonoBehaviour {
 	AudioSource doorRattleSFX;
 	AudioSource powerCollectSFX;
 
+	// Tutorial
+	TutorialTextScript movementText;
+	TutorialTextScript levitationText;
+	TutorialTextScript gravityText;
+	Vector3 movementTextPosition;
+	Vector3 levitationTextPosition;
+	Vector3 gravityTextPosition;
+
 	void Start() 
 	{
 		// cache references
 		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		player = GameObject.Find("Lucina");
 		playerScript = player.GetComponent<PlayerScript>();
+		movementText = GameObject.Find("MovementText").GetComponent<TutorialTextScript>();
+		levitationText = GameObject.Find("LevitationText").GetComponent<TutorialTextScript>();;
+		gravityText = GameObject.Find("GravityText").GetComponent<TutorialTextScript>();;
+
 		keyCollectSFX = GetComponents<AudioSource>()[11];
 		doorRattleSFX = GetComponents<AudioSource>()[12];
 		powerCollectSFX = GetComponents<AudioSource>()[13];
+
+		movementTextPosition = new Vector3(464.1634f, -0.582355f, 0.6604137f);
+		levitationTextPosition = new Vector3(481.7638f, -0.582355f, 0.660414f);
+		gravityTextPosition = new Vector3(506.1514f, -0.582355f, 0.660414f);
+
+		movementText.SetTutorialPosition(movementTextPosition);
+		movementText.SetTimeIn(3.0f);
+		movementText.EnterText();
 	}
 	
 	void Update() 
@@ -36,12 +56,20 @@ public class Level1Script : MonoBehaviour {
 			gameManager.SetHasLevitation(true);
 			Destroy(c.gameObject);
 			if (powerCollectSFX) powerCollectSFX.Play();
+
+			levitationText.SetTutorialPosition(levitationTextPosition);
+			levitationText.SetTimeIn(1.0f);
+			levitationText.EnterText();
 		}
 		else if (c.gameObject.name.Equals("GravityPower"))
 		{
 			gameManager.SetHasGravity(true);
 			Destroy(c.gameObject);
 			if (powerCollectSFX) powerCollectSFX.Play();
+
+			gravityText.SetTutorialPosition(gravityTextPosition);
+			gravityText.SetTimeIn(1.0f);
+			gravityText.EnterText();
 		}
 		else if (c.tag.Equals("Door"))
 		{
---
+++ Assets/Scripts/UI/TutorialTextScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTextScript : MonoBehaviour {
+
+	Vector3 tutorialPosition;
+	float timeIn;
+
+	void Start() 
+	{
+
+	}
+	
+	void Update() 
+	{
+		
+	}
+
+	public void EnterText()
+	{
+		iTween.MoveTo(gameObject, tutorialPosition,  timeIn);
+	}
+
+	public void SetTutorialPosition(Vector3 pos) { tutorialPosition = pos; }
+	public void SetTimeIn(float time) { timeIn = time; }
+}
