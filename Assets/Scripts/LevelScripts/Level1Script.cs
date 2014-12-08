08edb9cf4f5629baca50d518fe7a3303c449f25d
Judy Trinh <jtrinh74@gmail.com>
2014-12-07 21:57:41 -0500

    Added reticle title card

--- Assets/Scripts/LevelScripts/Level1Script.cs
+++ Assets/Scripts/LevelScripts/Level1Script.cs
@@ -17,9 +17,11 @@ public class Level1Script : MonoBehaviour {
 	TutorialTextScript movementText;
 	TutorialTextScript levitationText;
 	TutorialTextScript gravityText;
+	TutorialTextScript reticleText;
 	Vector3 movementTextPosition;
 	Vector3 levitationTextPosition;
 	Vector3 gravityTextPosition;
+	Vector3 reticleTextPosition;
 
 	void Start() 
 	{
@@ -28,8 +30,9 @@ public class Level1Script : MonoBehaviour {
 		player = GameObject.Find("Lucina");
 		playerScript = player.GetComponent<PlayerScript>();
 		movementText = GameObject.Find("MovementText").GetComponent<TutorialTextScript>();
-		levitationText = GameObject.Find("LevitationText").GetComponent<TutorialTextScript>();;
-		gravityText = GameObject.Find("GravityText").GetComponent<TutorialTextScript>();;
+		levitationText = GameObject.Find("LevitationText").GetComponent<TutorialTextScript>();
+		gravityText = GameObject.Find("GravityText").GetComponent<TutorialTextScript>();
+		reticleText = GameObject.Find("ReticleText").GetComponent<TutorialTextScript>();
 
 		keyCollectSFX = GetComponents<AudioSource>()[11];
 		doorRattleSFX = GetComponents<AudioSource>()[12];
@@ -37,6 +40,7 @@ public class Level1Script : MonoBehaviour {
 
 		movementTextPosition = new Vector3(464.1634f, -0.582355f, 0.6604137f);
 		levitationTextPosition = new Vector3(481.7638f, -0.582355f, 0.660414f);
+		reticleTextPosition = new Vector3(492.0935f, -0.070f, 0.660414f);
 		gravityTextPosition = new Vector3(506.1514f, -0.582355f, 0.660414f);
 
 		movementText.SetTutorialPosition(movementTextPosition);
@@ -82,6 +86,12 @@ public class Level1Script : MonoBehaviour {
 			{
 				if (doorRattleSFX) doorRattleSFX.Play();
 			}
+		}
+		else if (c.tag.Equals("ReticleTip"))
+		{
+			reticleText.SetTutorialPosition(reticleTextPosition);
+			reticleText.SetTimeIn(1.0f);
+			reticleText.EnterText();
 		}
 	}
 
